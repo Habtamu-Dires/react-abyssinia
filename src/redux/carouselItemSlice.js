fe4697add5293f42bdccaabbde7b81be788f1a6e
Habtamu <habtamuadugna.d@gmail.com>
2023-03-02 16:59:46 +0300

    redux updated to netlfy server url

--- src/redux/carouselItemSlice.js
+++ src/redux/carouselItemSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { baseUrl } from "../shared/baseUrl";
+//import { baseUrl } from "../shared/baseUrl";
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const initialState ={
     carouselItems: [],
